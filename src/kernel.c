113f622b218a5fb485a98a16ec2d0e83cbd33d50
mahanfr <mahan.farzaneh@gmail.com>
2024-06-16 02:13:04 +0330

    Adding Print Function

--- src/kernel.c
+++ src/kernel.c
@@ -1,24 +1,43 @@
 #include "../common/graphics.h"
 #include "../common/fonts.h"
 
-void PutChar(FrameBuffer* framebuffer,
-        PSF1_FONT* font, unsigned int color, char chr,
+#define WHITE 0xFFFFFFFF
+
+Point CursorPosition = {.x= 8, .y= 16};
+FrameBuffer frameBuffer;
+PSF1_FONT font;
+
+void PutChar(unsigned int color, char chr,
         unsigned int x_offset, unsigned int y_offset) {
-    unsigned int* pixPtr = (unsigned int*)framebuffer->BaseAddress;
-    char* fontPtr = font->glyphBuffer + (chr * font->psfHeader->charsize);
+    unsigned int* pixPtr = (unsigned int*)frameBuffer.BaseAddress;
+    char* fontPtr = font.glyphBuffer + (chr * font.psfHeader->charsize);
     for (unsigned long y = y_offset; y < y_offset+16; y++) {
         for (unsigned long x = x_offset; x< x_offset + 8; x++) {
             if ((*fontPtr & (0b10000000 >> (x - x_offset))) > 0){
-                *(unsigned int*)(pixPtr + x + (y * framebuffer->PixelPerScanLine)) = color;
+                *(unsigned int*)(pixPtr + x + (y * frameBuffer.PixelPerScanLine)) = color;
             }
         }
         fontPtr++;
     }
 }
 
-void _start(FrameBuffer* framebuffer, PSF1_FONT* font) {
-    PutChar(framebuffer, font, 0xFFFFFFFF, 'G', 10, 10);
-    PutChar(framebuffer, font, 0xFFFFFFFF, 'A', 20, 10);
-    PutChar(framebuffer, font, 0xFFFFFFFF, 'Y', 30, 10);
+void Print(char* str, unsigned int color) {
+    char* chr = str;
+    while (*chr != 0) {
+        PutChar(color, *chr, CursorPosition.x, CursorPosition.y);
+        CursorPosition.x += 8;
+        if (CursorPosition.x > frameBuffer.Width) {
+            CursorPosition.x = 8;
+            CursorPosition.y += 16;
+        }
+        chr++;
+    }
+}
+
+void _start(FrameBuffer* framebuffer, PSF1_FONT* psf_font) {
+    frameBuffer = *framebuffer;
+    font = *psf_font;
+
+    Print("Hello World From Kernel!\0", WHITE);
     return;
 }
