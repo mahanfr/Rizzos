40992c6cdf35a9cf5615f1302c6d25b6d60f5709
mahanfr <mahan.farzaneh@gmail.com>
2024-06-22 13:08:59 +0330

    Adding Global Descriptor Table structs

---
+++ src/gdt/gdt.h
@@ -0,0 +1,31 @@
+#ifndef _GDTable_
+#define _GDTable_
+
+#include <stdint.h>
+typedef struct {
+    uint16_t size;
+    uint32_t offset;
+} __attribute__((packed)) GDTDescriptor;
+
+typedef struct {
+    uint16_t limit0;
+    uint16_t base0; 
+    uint8_t base1;
+    uint8_t accessByte;
+    uint8_t limit1_flag;
+    uint8_t base2;
+} __attribute__((packed)) GDTEntry;
+
+typedef struct {
+    GDTEntry null;
+    GDTEntry kernelCode;
+    GDTEntry kernelData;
+    GDTEntry userNull;
+    GDTEntry userCode;
+    GDTEntry userData;
+} __attribute__((packed)) GDT
+__attribute__((aligned(0x1000)));
+
+extern GDT DefaultGDT;
+
+#endif
